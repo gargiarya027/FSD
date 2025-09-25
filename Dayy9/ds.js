996b0c30daa5e471d9f64c10edfd14ee36f0b55c
gargiarya027 <gargiarya161@gmail.com>
2025-09-25 15:51:10 +0530

    commit

---
+++ Dayy9/ds.js
@@ -0,0 +1,47 @@
+/*console.log("hello world");
+let a;
+comsole.log(a);
+a=12;
+var a=40;
+if(a>20){
+    var a=90;
+    console.log("a inside if="+a);
+
+}
+else{
+    console.log("hii...inside else");
+}
+console.log("a is outside"+a);
+
+let a=40;
+if(a>20){
+    let a=90;
+    console.log("a inside if="+a);
+
+}
+else{
+    console.log("hii...inside else");
+}
+console.log("a is outside"+a);
+
+
+function greetings(msg){
+    return  'hiiiiiii'+msg
+    console.log("ended");
+}
+const data=greetings("hellooo");
+console.log("data="+data);
+
+const data=function greetings(msg){
+    return "hiiiiii"+msg;
+
+}
+const msg=ata("abc");
+console.log(msg);
+
+const data=msg=>msg;
+console.log(data("hiiiii"));
+*/
+(function(){
+    console.log("hiiiiiii")
+})();
