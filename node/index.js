989fe4f220f681c7617c0308fa2873bdb46e55ed
gargiarya027 <gargiarya161@gmail.com>
2026-02-03 12:06:44 +0530

    commit

--- node/index.js
+++ node/index.js
@@ -1,9 +1,13 @@
+//ASYNC CODE
 const fs=require('fs');
-
-
 //fs.writeFileSync("./ds-b.txt","Hello Everyone")
 //fs.writeFileSync("./abes.txt","Hi ABES College students")
  //const result=fs.readFileSync("./abes.txt","utf-8");
 //console.log(result);
-const result=fs.readFileSync("./ds-b.txt","utf-8");
-console.log(result);
+//const result=fs.readFileSync("./ds-b.txt","utf-8");
+//console.log(result);
+
+
+//ASYNC CODE
+fs.appendFileSync("./abes.txt","phD");
+fs.appendFileSync("./abes.txt",`Gargi`,()=>{});
