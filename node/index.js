7e46a851fed6b2c572a6a14fc1b77051c400f51a
gargiarya027 <gargiarya161@gmail.com>
2026-02-17 11:06:40 +0530

    commit

--- node/index.js
+++ node/index.js
@@ -1,17 +1,32 @@
-const myPromise = new Promise((resolve, reject) => {
-  let success = true;
+//ASYNC CODE
+const fs=require('fs');
+//fs.writeFileSync("./ds-b.txt","Hello Everyone")
+//fs.writeFileSync("./abes.txt","Hi ABES College students")
+ //const result=fs.readFileSync("./abes.txt","utf-8");
+//console.log(result);
+//const result=fs.readFileSync("./ds-b.txt","utf-8");
+//console.log(result);
 
-  if (success) {
-    resolve("Promise resolved ✅");
-  } else {
-    reject("Promise rejected ❌");
-  }
+
+//ASYNC CODE
+
+////fs.appendFileSync("./abes.txt",PhD);
+
+//APPEND in async
+//fs.appendFile("./abes.txt",` Gargi`,()=>{ });
+
+//COPY in sync
+//fs.cpSync("./abes.txt","./a2.txt")
+//DELETE SYNC
+
+//fs.unlinkSync("./ds-b.txt")
+//DELETE ASYNC
+
+fs.unlink("./abes.txt",(err)=>{
+    if(err){
+        console.log("Error",err);
+    }
+    else{
+        console.log("Data Deleted");
+    }
 });
-
-myPromise
-  .then(result => {
-    console.log(result);
-  })
-  .catch(error => {
-    console.log(error);
-  });
